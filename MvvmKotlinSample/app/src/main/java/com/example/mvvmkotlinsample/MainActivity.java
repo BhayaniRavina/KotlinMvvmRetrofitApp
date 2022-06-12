de7f81bfa3452f0ac23e93aabdd0ba9e67c72865
BhayaniRavina <Ravina.bhayani8@gmail.com>
2022-06-12 17:25:20 +0530

    create basic structure

---
+++ MvvmKotlinSample/app/src/main/java/com/example/mvvmkotlinsample/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.mvvmkotlinsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //call recyclerview
+        //call recyclerview adapter
+
+        //retrive data from api form webservice using retrofit
+        //create pojo class for getting response
+
+        //store data into recyclerview
+    }
+}
